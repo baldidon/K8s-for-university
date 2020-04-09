6424c4c55685987e4474ac812b8609457a94d57c
baldidon <andreabaldinelli1998@gmail.com>
2020-04-09 11:30:15 +0200

    experiments with alpine-linux

--- tirocinio/tensorflow/modello_capi_abbigliamento.py
+++ tirocinio/tensorflow/modello_capi_abbigliamento.py
@@ -124,4 +124,65 @@ print('\nTest accuracy:', test_acc)
 #ora possiamo farci delle previsioni su altre immagini
 
 predictions = model.predict(test_images)
+#con questa funzione, teoricamente il modello dovrebbe aver previsto l'etichetta di ciascun immagine
+#il risultato è un vettore a dieci componenti, poichè mi esprime la probabilità che un'immagine stia in una delle nove classi di oggetti che abbiamo!
 
+#verifichiamo se la predizione dell'immagine 0 coincide con l'etichetta associata
+print(predictions[0])
+if np.argmax(predictions[0]) == test_labels[0]:
+    print("previsione corretta!")
+else:
+    print("modello non molto preciso")
+
+#ci sono alcuni casi che il modello è un po' più indeciso. In questo caso ci ha restituito una probabilità del 98 percento che fosse uno stivaletto(e quello era!!!)
+
+
+#==================================================================
+#per visualizzare le previsioni fatte
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
